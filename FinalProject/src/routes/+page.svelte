ad12d38954d5d42fbc703fd693d3dd1979df5a9f
Cpargy <cpargman@live.com>
2025-04-06 12:53:37 -0500

    Added clickable lake map

--- FinalProject/src/routes/+page.svelte
+++ FinalProject/src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
   import Background from '../components/background.svelte';
   import Introduction from '../components/introduction.svelte';
+  import MinnesotaMap from '../components/minnesotaMap.svelte';
+  import LakeStats from '../components/lakeStats.svelte';
+  let selectedLake;
 </script>
 
 <style>
@@ -25,14 +28,36 @@
     .intro {
         font-size: large;
     }
+    .map-box {
+    margin: 2rem auto;
+    width: 95%;
+    height: 1000px;
+    border: 2px solid white;
+    display: flex;
+    color: white;
+  }
+  .map-half {
+    flex: 1;
+    position: relative;
+  }
+  .stats-half {
+    flex: 1;
+    padding: 1rem;
+    background-color: rgba(255,255,255,0.1);
+  }
   </style>
 
-<div class = "title_intro">
-    <h1 class = "title">Factors that Influence Minnesota Fish Populations</h1>
-    <div class = "background-info">
-        <Background/>
-    </div>
-    <div class = "intro">
-        <Introduction/>
-    </div>
+<div class="title_intro">
+    <h1 class="title">Factors that Influence Minnesota Fish Populations</h1>
+    <div class="background-info"><Background/></div>
+    <div class="intro"><Introduction/></div>
+  
+    <div class="map-box">
+        <div class="map-half">
+          <MinnesotaMap bind:selectedLake />
+        </div>
+        <div class="stats-half">
+          <LakeStats {selectedLake} />
+        </div>
+      </div>
 </div>
